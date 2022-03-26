8459160511c7244f73664ae1f6f05cb8e9dc8047
Abizeyimana Victor <svicky.shema@gmail.com>
2022-03-26 20:41:30 +0200

    Remove Hardcoded values

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const server = require('./server');
 server.start({
-	backend: 'pg',
-	connectionParams: {
-		// db_path: 'test.db',
-		db_name: 'frappe_db',
-		username: 'victor',
-		password: '123456',
-		host: 'localhost',
-		client: 'pg',
-	},
+  backend: 'pg', // To start a postgres backend.
+  connectionParams: {
+    db_name: process.env.DB_NAME, // Existing postgres database name
+    username: process.env.DB_USERNAME, // Database username
+    password: process.env.DB_PWD, // Database password
+    host: process.env.DB_HOST, // Database host
+    client: 'pg', // Database client. options are [pg]
+  },
+  enableCORS: true,
+  models, // this will contain your database models
+  routes,
 });
